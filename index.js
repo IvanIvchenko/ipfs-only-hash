dbf9d81ebcf34b08196d5abbfa0ab42b776652d5
IvanIvchenko <32459492+IvanIvchenko@users.noreply.github.com>
2025-08-07 09:37:58 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,59 @@
-const { importer } = require('ipfs-unixfs-importer')
-
 const block = {
-  get: async cid => { throw new Error(`unexpected block API get for ${cid}`) },
-  put: async () => { throw new Error('unexpected block API put') }
-}
+  get: async (cid) => {
+    throw new Error(`unexpected block API get for ${cid}`);
+  },
+  put: async () => {
+    throw new Error('unexpected block API put');
+  },
+};
 
-exports.of = async (content, options) => {
-  options = options || {}
-  options.onlyHash = true
+async function hash(content_, options_) {
+  const { importer } = await import('ipfs-unixfs-importer');
+  const options = options_ || {};
+  options.onlyHash = true;
 
+  let content = content_;
   if (typeof content === 'string') {
-    content = new TextEncoder().encode(content)
+    content = [{ content: new TextEncoder().encode(content) }];
+  } else if (content instanceof Object.getPrototypeOf(Uint8Array)) {
+    content = [{ content }];
   }
 
-  let lastCid
-  for await (const { cid } of importer([{ content }], block, options)) {
-    lastCid = cid
+  let lastCID;
+  for await (const c of importer(content, block, options)) {
+    lastCID = c.cid;
   }
-
-  return `${lastCid}`
+  return lastCID;
 }
+
+async function hashFiles(path, options) {
+  const { globSource } = await import('ipfs-http-client');
+  options = {
+    cidVersion: 0, // Lines up with the smart contract code
+    hidden: true,
+    ...options,
+  };
+
+  const files = globSource(path, '**');
+
+  const rootCID = await hash(files, options);
+  return rootCID;
+}
+
+module.exports = {
+  cidToHex(cid) {
+    return `0x${Buffer.from(cid.bytes.slice(2)).toString('hex')}`;
+  },
+
+  of: hash,
+
+  async ofFile(path) {
+    return await hashFiles(path, {});
+  },
+
+  async ofDir(path) {
+    return await hashFiles(path, {
+      wrapWithDirectory: true,
+    });
+  },
+};
